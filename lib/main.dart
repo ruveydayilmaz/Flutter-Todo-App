d588322e9ee35f539662409840c29f90cbff7126
ruveydayilmaz <nur.yilmaz@bil.omu.edu.tr>
2022-02-17 22:23:41 +0300

    add dark theme/light theme

---
+++ lib/main.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+import 'package:get/get_navigation/src/root/get_material_app.dart';
+import 'package:get_storage/get_storage.dart';
+import 'package:to_do_app/services/theme_services.dart';
+import 'package:to_do_app/ui/home_page.dart';
+import 'package:to_do_app/ui/theme.dart';
+
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await GetStorage.init();
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return GetMaterialApp(
+      title: 'Flutter Demo',
+      debugShowCheckedModeBanner: false,
+      theme: Themes.light,
+      darkTheme: Themes.dark,
+      themeMode: ThemeService().theme,
+
+      home: HomePage()
+    );
+  }
+}
---
+++ lib/ui/theme.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class Themes {
+  static final light = ThemeData(
+    primaryColor: Colors.lightGreen,
+    brightness: Brightness.light
+  );
+
+  static final dark = ThemeData(
+    primaryColor: Colors.lightGreen,
+    brightness: Brightness.dark
+  );
+}
